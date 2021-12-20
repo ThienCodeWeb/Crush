f1dd183d77189325a560636ea565c7380a4ce843
ThienCodeWeb <96398917+ThienCodeWeb@users.noreply.github.com>
2021-12-20 09:02:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Nguyễn Phạm Đăng Khoa",
-    introTitle: 'Anh Khoa',
+    introTitle: 'Anh Thư',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
