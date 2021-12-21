1cff5a51a44ad18d387d51a84eb2994bb1e22fad
ThienCodeWeb <96398917+ThienCodeWeb@users.noreply.github.com>
2021-12-21 14:35:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
-    titleWeb: "Nguyễn Phạm Đăng Khoa",
-    introTitle: 'Anh Thư',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Phạm Nguyễn Anh Khoa",
+    introTitle: 'Phương Linh',
+    introDesc: `Yêu PLinh`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -16,5 +13,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/iwillcoder.203' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/anhkhoa.phamnguyen.1232' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
